d833c461714b8f1089d34babdd689e99cfc19eb8
MO99ME99 <m.m.hosseini1099@gmail.com>
2023-02-08 06:49:14 -0500

    007 Using the selected Date to display it and looking into SimpleDateFormat and Date

--- app/src/main/java/ir/iammrbit/ageinminutes/MainActivity.kt
+++ app/src/main/java/ir/iammrbit/ageinminutes/MainActivity.kt
@@ -1,11 +1,40 @@
 package ir.iammrbit.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener {
+            clickDatePicker(it)
+        }
+    }
+
+    private fun clickDatePicker(view : View){
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val dayOfMonth = myCalender.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this ,
+            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                Toast.makeText(this,
+                    "The chosen year is $selectedYear , the month is ${selectedMonth+1} and the day is $selectedDayOfMonth" ,
+                    Toast.LENGTH_SHORT).show()
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+                tvSelectedDate.text = selectedDate
+                val sdf = SimpleDateFormat("dd/MM/yyyy" , Locale.ENGLISH)
+                val theDate = sdf.parse(selectedDate)
+
+                                               },
+            year ,month,dayOfMonth ).show()
     }
 }
